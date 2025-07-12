706b7b9a9d77846ad0ae615f84c56fd3384fd2cf
KhondamirChess <hondamirkenjaboev@gmail.com>
2025-07-12 06:10:15 +0500

    second commit

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Set;
+
+public class Customer {
+    private final Long id;
+    private final String name;
+    private final Long level;
+    private final Set<Order> orders;
+
+    public Customer(Long id, String name, Long level, Set<Order> orders) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.orders = orders;
+    }
+}
---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private final Long id;
+    private final String name;
+    private final String category;
+    private final BigDecimal price;
+
+    public Product(Long id, String name, String category, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+}
